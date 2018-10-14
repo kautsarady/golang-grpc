f08d249ce73f856235674323e35f33e4c8dbefe4
kautsarady <kautsarady.dev@gmail.com>
2018-10-14 14:24:00 +0800

    setting testing unit

--- bulubulu/main.go
+++ bulubulu/main.go
@@ -1,6 +1,37 @@
 package bulubulu
 
+import (
+	"errors"
+	"strings"
+)
+
 //Translate translate bulubulu language to human language
-func Translate(t string) (string, error) {
-	return "Hello", nil
+func Translate(txt string) (string, error) {
+	if len(txt) == 0 {
+		return "", errors.New("Can't convert empty text")
+	}
+	buf := ""
+	for i := 0; i < len(txt); i++ {
+		ch := strings.ToLower(txt[i : i+1])
+		switch ch {
+		case "l":
+			buf += "b"
+			break
+		case "b":
+			buf += "l"
+			break
+		default:
+			buf += repVowel(ch)
+		}
+	}
+	return buf, nil
+}
+
+func repVowel(s string) string {
+	for _, v := range []string{"a", "i", "u", "e", "o"} {
+		if s == v {
+			return "u"
+		}
+	}
+	return s
 }
--- bulubulu/main_test.go
+++ bulubulu/main_test.go
@@ -1,11 +1,30 @@
 package bulubulu
 
 import (
+	"errors"
 	"testing"
 )
 
 func TestTranslate(t *testing.T) {
-	if res, _ := Translate(""); res != "Helo" {
-		t.Errorf("not expected output\n")
+	tt := []struct {
+		name, in, want string
+		err            error
+	}{
+		{"simple1", "lblbl", "blblb", nil},
+		{"simple2", "aiueo", "uuuuu", nil},
+		{
+			"complex",
+			"once upon of a time live a king without an heir",
+			"uncu upun uf u tumu buvu u kung wuthuut un huur",
+			nil,
+		},
+		{"empty string", "", "", errors.New("Can't convert empty text")},
+	}
+	for _, tc := range tt {
+		t.Run(tc.name, func(t *testing.T) {
+			if res, _ := Translate(tc.in); res != tc.want {
+				t.Errorf("Expected: %s; Got: %s", tc.want, res)
+			}
+		})
 	}
 }
